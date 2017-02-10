6c8fac8501a5e65b05bebe737f6cff420baa6f8d
Bussarin37 <infiniteelf.jeejeee@gmail.com>
2017-02-10 10:13:27 +0700

    Sign up

---
+++ app/src/main/java/appewtc/masterung/bsrufriend/MyAlert.java
@@ -0,0 +1,21 @@
+package appewtc.masterung.bsrufriend;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 10/2/2560.
+ */
+
+public class MyAlert {
+
+    //Explicit
+    private Context context; //context โยนData
+
+    public MyAlert(Context context) {
+        this.context = context; //กด ait+inset
+    }//Constructor
+
+
+
+
+}//Main Class
